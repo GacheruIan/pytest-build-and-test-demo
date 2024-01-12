eb8c5b7cae2755f228c234b3ffa089b7bd0b1829
ian gacheru <gacheruian99@gmail.com>
2024-01-12 21:27:24 +0300

    calculate test-pass

---
+++ calc.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+def word(name):
+    return f"Hello, {name}"
+
+def modulo(x,y):
+    return x%y 
